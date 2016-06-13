a5b29cfaae03c970d2c015d32621baa888595ec8
lucasmuniz <lucas_goedert@hotmail.com>
2016-06-13 20:59:53 -0300

    adicinando

---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.Dominio/Entidades/TipoServico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Si.Dev.Uniplac.TrabalhoSC.Dominio.Entidades
+{
+   public enum TipoServico
+    {
+       Revisao,
+       TrocaDeOleo,
+       TrocaDeVelas,
+       TrocaDeCorrea,
+       TrocaDeFluidoFreio,
+       TrocaRolamentoEmbreagem
+    };
+}
