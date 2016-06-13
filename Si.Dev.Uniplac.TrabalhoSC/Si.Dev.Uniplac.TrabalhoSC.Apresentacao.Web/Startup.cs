6f53f38474134241bb9df9416f1a0b8945638e8e
Giorgi Filipi Coelho D'ávila <giorgi_coelho@hotmail.com>
2016-06-13 20:49:46 -0300

    teste

---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.Apresentacao.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Si.Dev.Uniplac.TrabalhoSC.Apresentacao.Web.Startup))]
+namespace Si.Dev.Uniplac.TrabalhoSC.Apresentacao.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.Infra.Dados/Contexto/ClienteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Si.Dev.Uniplac.TrabalhoSC.Infra.Dados.Contexto
+{
+    class ClienteContext : DbContext
+    {
+        public ClienteContext() : base("ClienteDB")
+        {
+        }
+
+        public DbSet<Carro> Produtos { get; set; }
+        public DbSet<Servico> Clientes { get; set; }
+    }
+}
