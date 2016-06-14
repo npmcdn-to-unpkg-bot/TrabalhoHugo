6421a1821f0389ad66ae991159258358e3d66898
Giorgi Filipi Coelho D'ávila <giorgi_coelho@hotmail.com>
2016-06-13 21:05:34 -0300

    meu deus hame do ceu

---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Si.Dev.Uniplac.TrabalhoSC.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.API/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Si.Dev.Uniplac.TrabalhoSC.API.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
