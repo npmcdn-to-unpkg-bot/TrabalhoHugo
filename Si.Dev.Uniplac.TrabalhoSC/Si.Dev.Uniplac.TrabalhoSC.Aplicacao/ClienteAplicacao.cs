4737fcb3b7c90b8fce93964e530c9e917a9170e0
HiagoMorais <alerf_hiago81@hotmail.com>
2016-06-13 21:29:35 -0300

    Aplicacao

---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.Aplicacao/ClienteAplicacao.cs
@@ -0,0 +1,38 @@
+﻿using Si.Dev.Uniplac.TrabalhoSC.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Si.Dev.Uniplac.TrabalhoSC.Aplicacao
+{
+    public class ClienteAplicacao : IClienteAplicacao
+    {
+
+        public Cliente CriarCliente(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletarCliente(Dominio.Entidades.Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dominio.Entidades.Cliente BuscarCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dominio.Entidades.Cliente> BuscarTodasCliente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dominio.Entidades.Cliente AtualizarCliente(Dominio.Entidades.Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Si.Dev.Uniplac.TrabalhoSC/Si.Dev.Uniplac.TrabalhoSC.Aplicacao/IClienteAplicacao.cs
@@ -0,0 +1,18 @@
+﻿using Si.Dev.Uniplac.TrabalhoSC.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Si.Dev.Uniplac.TrabalhoSC.Aplicacao
+{
+    public interface IClienteAplicacao
+    {
+        Cliente CriarCliente(Cliente cliente);
+        void DeletarCliente(Cliente cliente);
+        Cliente BuscarCliente(int id);
+        List<Cliente> BuscarTodasCliente();
+        Cliente AtualizarCliente(Cliente cliente);
+    }
+}
